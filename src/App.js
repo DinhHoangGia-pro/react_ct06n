0d51d5868cf069026e2882b9697659486034f5a8
DinhHoangGia-pro <dinhhoanggia@gmail.com>
2024-10-21 08:52:53 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
 
-function App() {
-  var tenlop="Xin chao cac ban";
+const App=()=> {
+  var tenlop="Xin chao cac ban ct06n";
+  var arr1=[{id:1, hoten:'Nguyen Van A', lop:'CT06N', diachi:'17A Cong Hoa'},  
+    {id:2, hoten:'Nguyen Van A', lop:'CT06N', diachi:'17A Cong Hoa'}, 
+    {id:3, hoten:'Nguyen Van A', lop:'CT06N', diachi:'17A Cong Hoa'}, 
+    {id:4, hoten:'Nguyen Van A', lop:'CT06N', diachi:'17A Cong Hoa'}, 
+    {id:5, hoten:'Nguyen Van A', lop:'CT06N', diachi:'17A Cong Hoa'}
+
+  ];
+
   return (
     <div>
-      {tenlop}
+    {tenlop}
+    {
+         arr1.map(phantu=>{
+          return (
+            <p>id: {phantu.id}, hoten: {phantu.hoten}, lop: {phantu.lop}, diachi: {phantu.diachi} 
+            
+            </p>
+          );
+         })
+    }
 
-    </div>
+    </div>    
 
   );
 }
+
+
 
 export default App;
